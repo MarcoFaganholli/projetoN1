1fd4cd2500d29211eda0871aa6701a104fc1d5fd
MarcoFaganholli <65420368+MarcoFaganholli@users.noreply.github.com>
2021-10-16 18:23:01 -0300

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,57 @@
+def adicionar_elemento(lista: list, elemento: str) -> bool:
+    '''
+    Verifica se o elemento começa com letra maiúscula e possui apenas letras e espaços.
+    Se ele passar na verificação, inserir na lista e retornar verdadeiro, caso contrário, retorne falso
+    '''
+
+
+def buscar_elemento(lista: list, elemento: str) -> bool:
+    '''
+    Verifica se um elemento está contido na lista.
+    Se estiver, retorne verdadeiro, caso contrário, retorne falso.
+    '''
+
+
+def remover_elemento(lista: list, elemento: str) -> bool:
+    '''
+    Verifica se a lista contém um elemento específico.
+    Se ele estiver contido na lista, remover o elemento e
+    retornar verdadeiro, caso contrário, retornar falso.
+    '''
+
+
+def limpar_lista(lista: list) -> None:
+    '''
+    Remove todos os elementos da lista.
+    Função sem retorno.
+    '''
+
+
+def ordenar_lista(lista: list) -> None:
+    '''
+    Ordena todos os elementos da lista por ordem
+    alfabética. A função não possui retorno
+    '''
+
+
+def pegar_quantidade(lista: list) -> int:
+    '''
+    Retorna a quantidade de elementos dentro
+    da lista
+    '''
+
+
+def converter_maiusculo(lista: list) -> list:
+    '''
+    Converte todos os elementos da lista para letra
+    maiúscula e os retorna em uma nova lista
+    '''
+
+
+def eliminar_repetidos(lista: list) -> list:
+    '''
+    DESAFIO: 0.5 extra
+    Remove todos os elementos repetidos na lista
+    e retorna uma lista nova
+    (não vale utilizar o set)
+    '''
